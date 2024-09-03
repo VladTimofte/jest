0bd0ead1482fe79d33268f92dd9fc18fbf26b7fe
Constantin Stoica <161923684+Stoica-Constantin27@users.noreply.github.com>
2024-09-03 20:29:14 +0300

    init js

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -9,12 +9,38 @@ import {
 describe("Utils functions", () => {
   test("should greet the user with it's name", () => {
     expect(greet("Alice")).toMatch("Hello, Alice!");
-
     expect(greet("Alice")).toBeTruthy();
     expect(greet("Alice")).not.toBeFalsy();
     expect(greet("Alice")).not.toBeNull();
     expect(greet("Alice")).not.toBeUndefined();
   });
 
-  test("should add two numbers corectly",)
+  test("should add two numbers corectly", () => {
+    expect(add(10, 15)).toBe(25);
+    expect(add("10", "15")).toBe(25);
+    expect(add(-10, -15)).toBe(25);
+
+    expect(add(-10, -15)).toBeGreaterThan(0);
+    expect(add(-10, -15)).toBeGreaterThanOrEqual(25);
+    expect(add(-10, -15)).toBeLessThan(30);
+    expect(add(-10, -15)).toBeLessThanOrEqual(30);
+  });
+
+  test("should multiply two numbers corectly", () => {
+    expect(multiply(2, 3)).toBe(6);
+    expect(multiply(2, 0)).toBe(0);
+  });
+
+  test("should create a product object", () => {
+    const mockProduct = {
+      productName: "ciocolata",
+      unitPrice: 10,
+      totalProducts: 50,
+      totalPrice: 500,
+    };
+    expect(createProductObject("ciocolata", 10, 50, 500)).toEqual(mockProduct);
+    expect(createProductObject("ciocolata", "10", 50, 500)).toEqual(
+      mockProduct
+    );
+  });
 });
